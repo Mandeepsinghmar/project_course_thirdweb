5bd6e9045ee841367ccdc074e6ffa4eba56290c0
Mandeep Singhmar <msingh8328@gmail.com>
2022-09-21 20:53:27 +0530

    fix right sidebar

--- components/RightSidebar.js
+++ components/RightSidebar.js
@@ -4,7 +4,7 @@ import CourseCard from './CourseCard';
 const RightSidebar = () => {
   return (
     <div className='bg-white text-gray-400 w-full flex gap-4 ml-36 m-4 rounded-3xl '>
-      <div className='bg-[#F9F9F9] p-8 max-w-[800px] rounded-l-3xl '>
+      <div className='bg-[#F9F9F9] p-8 max-w-[800px] rounded-l-3xl order-2 lg:order-none '>
         <div className='flex justify-between'>
           <p className='font-bold text-2xl'>Courses</p>
           <p className='bg-white p-2 rounded-xl'>
@@ -63,7 +63,7 @@ const RightSidebar = () => {
         </div>
       </div>
 
-      <div className='p-8'>
+      <div className='p-8 order-1 lg:order-none'>
         <img
           className='w-full h-[500px]'
           src='/images/photograph.svg'
@@ -78,11 +78,7 @@ const RightSidebar = () => {
                 fill='currentColor'
                 class='w-5 h-5 text-yellow-400'
               >
-                <path
-                  fill-rule='evenodd'
-                  d='M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z'
-                  clip-rule='evenodd'
-                />
+                <path d='M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z' />
               </svg>
             ))}
             {[...Array(2)].map(() => (
@@ -92,11 +88,7 @@ const RightSidebar = () => {
                 fill='currentColor'
                 class='w-5 h-5 text-gray-300'
               >
-                <path
-                  fill-rule='evenodd'
-                  d='M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z'
-                  clip-rule='evenodd'
-                />
+                <path d='M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z' />
               </svg>
             ))}
             <p className='ml-4'>
